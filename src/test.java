06bc4fb1319b882eeaa75c3bef67c17c38328719
HWANGDEOK <dukjun2002@naver.com>
2025-10-15 11:36:40 +0900

    test.java 파일 수정3

--- src/test.java
+++ src/test.java
@@ -1,9 +1,4 @@
 public class Test{
 	public static void main(String[] args) {
 		System.out.println("test 출력");
-		System.out.println("test 출력");
-		System.out.println("test 출력");
-		System.out.println("test 출력");
-		System.out.println("test 출력");
-		System.out.println("test 출력");
 	}
